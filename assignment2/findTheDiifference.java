d43b1e42d54d24380c0c857b63ce324f99bbe38e
rifas <mohamedrifasrifas27@gmail.com>
2026-07-23 10:11:10 +0530

    day25

---
+++ assignment2/findTheDiifference.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public char findTheDifference(String s, String t) {
+        // Initialize variables to store sum of ASCII codes for 
+        // each string
+        int charCodeS = 0, charCodeT = 0;
+        // Iterate through both strings and char codes
+        for (int i = 0; i < s.length(); ++i) charCodeS += (int)s.charAt(i);
+        for (int i = 0; i < t.length(); ++i) charCodeT += (int)t.charAt(i);
+        // Return the difference between 2 strings as char
+        return (char)(charCodeT - charCodeS);
+    }
+}
+
+
+ 
+"1 optimization: As t.length() is just 1 character longer than s.length(), we can use 1 pass to process both strings (20% better runtime performance)."
+public char findTheDifference(String s, String t) {
+        int charCode = t.charAt(s.length());
+        // Iterate through both strings and char codes
+        for (int i = 0; i < s.length(); ++i) {
+              charCode -= (int)s.charAt(i);
+              charCode += (int)t.charAt(i); 
+        }
+        return (char)charCode;
+    }
---
+++ assignment2/vowelStrings.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[] vowelStrings(String[] words, int[][] queries) {
+        int n=words.length;
+        int m=queries.length;
+        int[] ans=new int[m];
+
+        // Set of vowels
+        Set<Character> set=new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u'));;
+
+
+        // Compute prefix sum array
+        int[] sum=new int[n];
+        sum[0]=( set.contains(words[0].charAt(0)) && set.contains(words[0].charAt(words[0].length()-1)) )?1:0;
+        for(int i=1; i<n; i++){
+            if( set.contains(words[i].charAt(0)) && set.contains(words[i].charAt(words[i].length()-1)) ){
+                sum[i]=sum[i-1]+1;
+            }
+            else{
+                sum[i]=sum[i-1];
+            }
+        }
+
+
+        // Process queries
+        int prev=0;
+        for(int i=0; i<m; i++){
+            prev=queries[i][0]-1<0?0:sum[queries[i][0]-1];
+            ans[i]=sum[queries[i][1]] - prev;
+        }
+
+        return ans;
+    }
+}
